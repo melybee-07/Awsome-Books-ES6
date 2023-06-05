668e4cc1f2848b8f5944315f1bd62a0df05a8658
MELISSA MAZIR <melissamazir07@gmail.com>
2023-06-05 22:40:14 +0100

    adding style

--- modules/book.js
+++ modules/book.js
@@ -1,25 +1,25 @@
 export default class Book {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.books = JSON.parse(localStorage.getItem("books")) || [];
   }
 
   renderBookList = () => {
-    const bookListElement = document.getElementById('bookList');
-    bookListElement.innerHTML = '';
+    const bookListElement = document.getElementById("bookList");
+    bookListElement.innerHTML = "";
 
     if (this.books.length === 0) {
-      bookListElement.innerHTML = 'No books in the collection.';
+      bookListElement.innerHTML = "No books in the collection.......";
       return;
     }
 
-    const ul = document.createElement('ul');
+    const ul = document.createElement("ul");
     this.books.forEach((book, index) => {
-      const li = document.createElement('li');
+      const li = document.createElement("li");
       li.innerHTML = `"${book.title}" by ${book.author} `;
 
-      const removeButton = document.createElement('button');
-      removeButton.innerText = 'Remove';
-      removeButton.addEventListener('click', () => this.removeBook(index));
+      const removeButton = document.createElement("button");
+      removeButton.innerText = "Remove";
+      removeButton.addEventListener("click", () => this.removeBook(index));
 
       li.appendChild(removeButton);
       ul.appendChild(li);
@@ -31,13 +31,13 @@ export default class Book {
   addBook = (title, author) => {
     const newBook = { title, author };
     this.books.push(newBook);
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
     this.renderBookList();
   };
 
   removeBook = (index) => {
     this.books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
     this.renderBookList();
   };
 }
