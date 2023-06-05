c6b0f22ceeff1c8659bff81d98edec4bafa33324
MELISSA MAZIR <melissamazir07@gmail.com>
2023-06-05 18:52:06 +0100

    Display the current date and time below the navigation bar and fixing linters errors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import Book from "./modules/book.js";
-import Navigation from "./modules/navigation.js";
-import Form from "./modules/form.js";
+/* global luxon */
+import Book from './modules/book.js';
+import Navigation from './modules/navigation.js';
+import Form from './modules/form.js';
 
+// eslint-disable-next-line no-unused-vars
 const bookCollection = new Book();
 
 // Initialize modules
@@ -10,3 +12,14 @@ Form.initialize(bookCollection);
 
 // Render initial content
 bookCollection.renderBookList();
+
+// Display current date and time
+const displayDateTime = () => {
+  const dateTimeElement = document.getElementById('dateTime');
+  dateTimeElement.textContent = luxon.DateTime.now().toLocaleString(
+    luxon.DateTime.DATETIME_FULL,
+  );
+};
+
+setInterval(displayDateTime, 1000);
+displayDateTime();
--- modules/form.js
+++ modules/form.js
@@ -1,17 +1,17 @@
 export default class Form {
   static initialize = (bookCollection) => {
     // Handle form submission
-    document.getElementById("bookForm").addEventListener("submit", (e) => {
+    document.getElementById('bookForm').addEventListener('submit', (e) => {
       e.preventDefault();
-      const titleInput = document.getElementById("title");
-      const authorInput = document.getElementById("author");
+      const titleInput = document.getElementById('title');
+      const authorInput = document.getElementById('author');
       const title = titleInput.value.trim();
       const author = authorInput.value.trim();
 
       if (title && author) {
         bookCollection.addBook(title, author);
-        titleInput.value = "";
-        authorInput.value = "";
+        titleInput.value = '';
+        authorInput.value = '';
       }
     });
   };
